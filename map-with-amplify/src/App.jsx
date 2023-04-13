5137faa8d9ed5c5a92abd9eec6bb0e3ae4954667
Ahmad Azizi <91204996+its-aazizi@users.noreply.github.com>
2023-04-13 20:55:00 +0500

    fixed build issues by adding react-map-gl version

--- map-with-amplify/src/App.jsx
+++ map-with-amplify/src/App.jsx
@@ -1,8 +1,8 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
 
-import { MapView } from "@aws-amplify/ui-react";
-import { NavigationControl } from "react-map-gl";
+import {MapView} from "@aws-amplify/ui-react";
+import {NavigationControl} from "react-map-gl";
 
 // Include MapLibre CSS for common elements like the NavigationControl
 import "maplibre-gl/dist/maplibre-gl.css";
@@ -11,15 +11,19 @@ import "maplibre-gl/dist/maplibre-gl.css";
 import "./config";
 
 export default () => (
-  <MapView
-    // See https://ui.docs.amplify.aws/react/connected-components/geo#mapview
-    initialViewState={{
-      latitude: 31.0,
-      longitude: 0.0,
-      zoom: 2,
-    }}
-  >
-    {/* See https://visgl.github.io/react-map-gl/docs/api-reference/navigation-control */}
-    <NavigationControl position="bottom-right" showZoom showCompass={false} />
-  </MapView>
+      <MapView
+            // See https://ui.docs.amplify.aws/react/connected-components/geo#mapview
+            initialViewState={{
+                  latitude: 31.0,
+                  longitude: 0.0,
+                  zoom: 2,
+            }}
+      >
+            {/* See https://visgl.github.io/react-map-gl/docs/api-reference/navigation-control */}
+            <NavigationControl
+                  position="bottom-right"
+                  showZoom
+                  showCompass={false}
+            />
+      </MapView>
 );
