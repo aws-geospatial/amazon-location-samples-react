d6774e4bf6e43026065b92c7584337a8c32da185
Ahmad Azizi <91204996+its-aazizi@users.noreply.github.com>
2024-10-01 19:08:20 +0500

    updated map-with-geojson sample, CF template update pending

--- map-with-geojson/src/App.jsx
+++ map-with-geojson/src/App.jsx
@@ -1,35 +1,37 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
 
-import Map, { NavigationControl } from "react-map-gl/maplibre";
-import { withIdentityPoolId } from "@aws/amazon-location-utilities-auth-helper";
-
+import Map, {NavigationControl} from "react-map-gl/maplibre";
 import "maplibre-gl/dist/maplibre-gl.css";
-
 import VancouverOverlay from "./VancouverOverlay";
 
-const identityPoolId = import.meta.env.VITE_IDENTITY_POOL_ID;
+const apiKey = import.meta.env.VITE_API_KEY;
 const region = import.meta.env.VITE_REGION;
-const mapName = import.meta.env.VITE_MAP_NAME;
-
-const authHelper = await withIdentityPoolId(identityPoolId);
 
 export default () => (
-  <Map
-    // See https://visgl.github.io/react-map-gl/docs/api-reference/map
-    initialViewState={{
-      latitude: 49.2819,
-      longitude: -123.1187,
-      zoom: 11,
-    }}
-    style={{ height: "100vh", width: "100vw" }}
-    mapStyle={`https://maps.geo.${region}.amazonaws.com/maps/v0/maps/${mapName}/style-descriptor`}
-    {...authHelper.getMapAuthenticationOptions()}
-  >
-    {/* See https://visgl.github.io/react-map-gl/docs/api-reference/navigation-control */}
-    <NavigationControl position="bottom-right" showZoom showCompass={false} />
+      <Map
+            // See https://visgl.github.io/react-map-gl/docs/api-reference/map
+            initialViewState={{
+                  latitude: 49.2509,
+                  longitude: -123.1147,
+                  zoom: 11,
+            }}
+            style={{height: "100vh", width: "100vw"}}
+            mapStyle={`https://maps.geo.${region}.amazonaws.com/v2/styles/Standard/descriptor?key=${apiKey}&color-scheme=Light`}
+            transformRequest={(url) =>
+                  url.indexOf("?key=") > -1
+                        ? {url: url}
+                        : {url: url + `?key=${apiKey}`}
+            }
+      >
+            {/* See https://visgl.github.io/react-map-gl/docs/api-reference/navigation-control */}
+            <NavigationControl
+                  position="bottom-right"
+                  showZoom
+                  showCompass={false}
+            />
 
-    {/* Display the city of Vancouver as a polygon overlay */}
-    <VancouverOverlay />
-  </Map>
+            {/* Display the city of Vancouver as a polygon overlay */}
+            <VancouverOverlay />
+      </Map>
 );
