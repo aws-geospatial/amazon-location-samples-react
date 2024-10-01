b3b9f633f8e9bdab52bbe2b1829f8fda171b8529
Ahmad Azizi <91204996+its-aazizi@users.noreply.github.com>
2024-10-01 20:28:39 +0500

    updated map-with-markers sample, CF template update pending

--- map-with-markers/src/App.jsx
+++ map-with-markers/src/App.jsx
@@ -1,54 +1,55 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
 
-import Map, { NavigationControl } from "react-map-gl/maplibre";
-import { withIdentityPoolId } from "@aws/amazon-location-utilities-auth-helper";
-
+import Map, {NavigationControl} from "react-map-gl/maplibre";
 import "maplibre-gl/dist/maplibre-gl.css";
-
 // Amazon Hub Lockers in Vancouver as a GeoJSON FeatureCollection
 import lockerGeoJSON from "./lockers.json";
-
 // React Component that renders markers for all provided lockers
 import LockerMarkers from "./LockerMarkers";
 
-const identityPoolId = import.meta.env.VITE_IDENTITY_POOL_ID;
+const apiKey = import.meta.env.VITE_API_KEY;
 const region = import.meta.env.VITE_REGION;
-const mapName = import.meta.env.VITE_MAP_NAME;
-
-const authHelper = await withIdentityPoolId(identityPoolId);
 
 // transform GeoJSON features into simplified locker objects
 const lockers = lockerGeoJSON.features.map(
-  ({
-    geometry: {
-      coordinates: [longitude, latitude],
-    },
-    properties: { title, address: description },
-  }) => ({
-    latitude,
-    longitude,
-    title,
-    description,
-  })
+      ({
+            geometry: {
+                  coordinates: [longitude, latitude],
+            },
+            properties: {title, address: description},
+      }) => ({
+            latitude,
+            longitude,
+            title,
+            description,
+      })
 );
 
 export default () => (
-  <Map
-    // See https://visgl.github.io/react-map-gl/docs/api-reference/map
-    initialViewState={{
-      latitude: 49.2819,
-      longitude: -123.1187,
-      zoom: 11,
-    }}
-    style={{ height: "100vh", width: "100vw" }}
-    mapStyle={`https://maps.geo.${region}.amazonaws.com/maps/v0/maps/${mapName}/style-descriptor`}
-    {...authHelper.getMapAuthenticationOptions()}
-  >
-    {/* See https://visgl.github.io/react-map-gl/docs/api-reference/navigation-control */}
-    <NavigationControl position="bottom-right" showZoom showCompass={false} />
+      <Map
+            // See https://visgl.github.io/react-map-gl/docs/api-reference/map
+            initialViewState={{
+                  latitude: 49.2509,
+                  longitude: -123.1147,
+                  zoom: 11,
+            }}
+            style={{height: "100vh", width: "100vw"}}
+            mapStyle={`https://maps.geo.${region}.amazonaws.com/v2/styles/Standard/descriptor?key=${apiKey}&color-scheme=Light`}
+            transformRequest={(url) =>
+                  url.indexOf("?key=") > -1
+                        ? {url: url}
+                        : {url: url + `?key=${apiKey}`}
+            }
+      >
+            {/* See https://visgl.github.io/react-map-gl/docs/api-reference/navigation-control */}
+            <NavigationControl
+                  position="bottom-right"
+                  showZoom
+                  showCompass={false}
+            />
 
-    {/* Render markers for all lockers, with a popup for the selected locker */}
-    <LockerMarkers lockers={lockers} />
-  </Map>
+            {/* Render markers for all lockers, with a popup for the selected locker */}
+            <LockerMarkers lockers={lockers} />
+      </Map>
 );
