b0692de4fdaee831966d7c2cc94a651e5337ece5
Ahmad Azizi <91204996+its-aazizi@users.noreply.github.com>
2024-10-02 18:31:08 +0500

    updated tracking-data-streaming sample, CF template update pending

--- tracking-data-streaming/src/configuration.js
+++ tracking-data-streaming/src/configuration.js
@@ -3,13 +3,14 @@
 
 // This configuration file is a single place to provide any values to set up the app
 
-export const READ_ONLY_IDENTITY_POOL_ID= "<your read only identity pool id>"; // REQUIRED - Amazon Cognito id for readonly role
-export const WRITE_ONLY_IDENTITY_POOL_ID= "<your write only identity pool id>"; // REQUIRED - Amazon Cognito id for writeonly role
-export const REGION = "us-west-2"; // REQUIRED - Amazon Cognito Region
+export const READ_ONLY_IDENTITY_POOL_ID = "<your read only identity pool id>"; // REQUIRED - Amazon Cognito id for readonly role
+export const WRITE_ONLY_IDENTITY_POOL_ID = "<your write only identity pool id>"; // REQUIRED - Amazon Cognito id for writeonly role
+export const REGION = "<your aws region>"; // REQUIRED - Amazon Cognito Region
+export const API_KEY = "<your api key>"; // REQUIRED - Amazon Location API key
 
 export const MAP = {
-  NAME: "TrackingAndGeofencingSampleMapHere", // REQUIRED - Amazon Location Service map resource name
-  STYLE: "VectorHereExplore", // REQUIRED - String representing the style of map resource
+      STYLE: "Standard", // REQUIRED - String representing the style of map resource
+      COLOR_SCHEME: "Light", // REQUIRED - String representing the color scheme of map resource
 };
 
 export const GEOFENCE = "TrackingAndGeofencingSampleCollection"; // REQUIRED - Amazon Location Service geofence collection resource name
@@ -18,4 +19,5 @@ export const TRACKER = "SampleTracker"; // REQUIRED - Amazon Location Service tr
 
 export const DEVICE_POSITION_HISTORY_OFFSET = 3600; // REQUIRED - Relative time range of Device Position History to display in seconds. Default to 1 hour.
 
-export const KINESIS_DATA_STREAM_NAME = "TrackingAndGeofencingSampleKinesisDataStream"; // REQUIRED for running the demo - defined in ./cfn_template/kinesisResources.yml
+export const KINESIS_DATA_STREAM_NAME =
+      "TrackingAndGeofencingSampleKinesisDataStream"; // REQUIRED for running the demo - defined in ./cfn_template/kinesisResources.yml
